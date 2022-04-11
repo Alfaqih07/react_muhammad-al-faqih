d5c89bf23f4b34862d2bfa20d5d115a7555b8bb3
Alfaqih07 <muhammad.alfaqih.100700@gmail.comn>
2022-04-11 13:10:24 +0700

    late post react hooks

--- react_latihan/src/App.js
+++ react_latihan/src/App.js
@@ -8,37 +8,42 @@ import { store, persistor} from './kantor/kantor'
 // import FunctionComponent from './FunctionComponent';
 
 
-// import Home from './Pages/Home'; 
+import Home from './Pages/Home'; 
 import About_app from './Pages/About_app'
 import About_author from './Pages/About_author'
 import { BrowserRouter as Deklarasi, Routes, Route } from 'react-router-dom';
 
 
 // import Home from './EventHandling/Home'  
-import Home from './Redux/Home'
+// -----eventHandling------
+
+
+// import Home from './Redux/Home' 
+// -----react redux-----
+
 import Form from './form/Form'
 
 function App() {
   return (
-    // <Deklarasi>
-    //   <Routes>
-    //     {/* <Route path="/" element={</>}/> */}
-    //       <Route index element={<Home />} />
-    //       <Route path="About_app" element={<About_app/>} />
-    //       <Route path="About_author" element={<About_author/>} />
-    //       {/* <Route path="*" element={<NoPage />} /> */} 
-    //   </Routes>
-    // </Deklarasi>  
+    <Deklarasi>
+      <Routes>
+        {/* <Route path="/" element={</>}/> */}
+          <Route index element={<Home />} />
+          <Route path="About_app" element={<About_app/>} />
+          <Route path="About_author" element={<About_author/>} />
+          {/* <Route path="*" element={<NoPage />} /> */} 
+      </Routes>
+    </Deklarasi>  
 
     
-    <div className='App'> 
-    <Provider store = {store}> 
-      <PersistGate loading={null} persistor ={persistor}>
-    {/* <FunctionComponent Users={Data}/> */} 
-      <Home/>
-      </PersistGate>
-    </Provider>
-    </div>
+    // <div className='App'> 
+    // <Provider store = {store}> 
+    //   <PersistGate loading={null} persistor ={persistor}>
+    // {/* <FunctionComponent Users={Data}/> */} 
+    //   <Home/>
+    //   </PersistGate>
+    // </Provider>
+    // </div>
 
   );
 }
