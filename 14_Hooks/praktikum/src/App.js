906ec3ee8485c6b8dd6fe7f5d529bad640322994
Alfaqih07 <muhammad.alfaqih.100700@gmail.comn>
2022-05-30 19:40:36 +0700

    revisi reactHooks

---
+++ 14_Hooks/praktikum/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';  
+import Classcomponent from './Classcomponent'; 
+import FunctionComponent from './FunctionComponent';
+// import Home from './Pages/Home'; 
+import About_app from './Pages/About_app'
+import About_author from './Pages/About_author'
+import { BrowserRouter as Deklarasi, Routes, Route } from 'react-router-dom';
+import Todos from './EventHandling/todo/Todo' 
+import Home from './EventHandling/Home'
+
+function App() {
+  return (
+    <Deklarasi>
+      <Routes>
+        {/* <Route path="/" element={</>}/> */}
+          <Route index element={<Home />} />
+          <Route path="About_app" element={<About_app/>} />
+          <Route path="About_author" element={<About_author/>} />
+          {/* <Route path="*" element={<NoPage />} /> */} 
+      </Routes>
+    </Deklarasi>  
+
+    
+    // <div className='App'>
+    // {/* <FunctionComponent Users={Data}/> */} 
+    // {/* <Todos /> */} 
+    // <Home/>
+    // </div>
+
+  );
+}
+
+export default App;
